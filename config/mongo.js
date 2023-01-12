51753130b23b3e53d51a72fcd4381e49c990d7de
Cameron Smith <805camerons@gmail.com>
2023-01-11 22:20:28 -0500

    Remove deprecated connect args

--- config/mongo.js
+++ config/mongo.js
@@ -6,9 +6,7 @@ class MongooseConnection {
             try {
                 const client = await mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`, {
                     useNewUrlParser: true,
-                    useUnifiedTopology: true,
-                    useCreateIndex: true,
-                    useFindAndModify: false
+                    useUnifiedTopology: true
                 });
                 resolve(client);
             } catch(error) {
